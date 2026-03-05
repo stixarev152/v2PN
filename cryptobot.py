0fb38afef2def09bb126ec0a362d3cbc022fbf62
stixarev152 <stixarev152@mail.ru>
2026-03-05 18:45:19 +0300

    Update cryptobot.py

--- cryptobot.py
+++ cryptobot.py
@@ -1,35 +1,5 @@
-import requests
-from config import CRYPTO_TOKEN
-
-API = "https://pay.crypt.bot/api"
-
+# cryptobot.py
 
 def create_invoice(amount):
-
-    headers = {
-        "Crypto-Pay-API-Token": CRYPTO_TOKEN
-    }
-
-    data = {
-        "asset":"USDT",
-        "amount":amount
-    }
-
-    r = requests.post(API+"/createInvoice",headers=headers,json=data).json()
-
-    return r["result"]["pay_url"], r["result"]["invoice_id"]
-
-
-def check_invoice(invoice_id):
-
-    headers = {
-        "Crypto-Pay-API-Token": CRYPTO_TOKEN
-    }
-
-    r = requests.get(API+"/getInvoices",headers=headers).json()
-
-    for i in r["result"]["items"]:
-        if i["invoice_id"] == invoice_id:
-            return i["status"]
-
-    return "active"
+    # заглушка
+    return f"https://pay.example.com/{amount}"
